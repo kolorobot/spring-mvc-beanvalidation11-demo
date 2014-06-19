a43c17b3ee3147e8cd77d6c1902d069e9f2d6b1e
kolorobot <rafal.borowiec@gmail.com>
2014-06-19 20:02:10 +0200

    Changed packaging type to jar, to allow running with embedded Tomcat.

--- src/main/java/pl/codeleak/demo/Application.java
+++ src/main/java/pl/codeleak/demo/Application.java
@@ -1,5 +1,6 @@
 package pl.codeleak.demo;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -8,5 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
-
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
 }
